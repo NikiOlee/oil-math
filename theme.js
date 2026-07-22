e62d461e3cad722102b95f87590535ed62a064bc
Nikita <nikitakorch08@gmail.com>
2026-07-22 21:58:30 +0200

    Transoil - best oil

---
+++ theme.js
@@ -0,0 +1,18 @@
+const themeBtn = document.querySelector('.themeBtn');
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-theme');
+  themeBtn.textContent = '☀️';
+}
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+
+  if (document.body.classList.contains('dark-theme')) {
+    themeBtn.textContent = '🌞';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    themeBtn.textContent = '🌝';
+    localStorage.setItem('theme', 'light');
+  }
+});
